99e04cb92ae7743a8b506c4267f076fe53819b42
Samwasee <samwaseee@gmail.com>
2025-05-06 23:36:28 +0600

    strings

--- string_operations.py
+++ string_operations.py
@@ -17,3 +17,28 @@ print("expension" in a)
 print(a.upper())
 print(a.lower())
 
+
+b = "    hello, world    "
+print(b.strip())  # removes leading and trailing whitespace
+print(b.lstrip())  # removes leading whitespace
+print(b.rstrip())  # removes trailing whitespace
+
+print(b.replace("hello", "hi"))  # replaces all occurrences of "hello" with "hi"
+print(b.split(","))  # splits the string into a list of substrings using "," as the delimiter
+
+print(a+" " + b)  # concatenates two strings
+print(a*3)  # repeats the string 3 times
+
+age = 25   
+txt = "My name is John, and I am {} years old".format(age)  # using format method
+print(txt)
+
+price = 49.99
+txt = f"My name is John, and I am {age} years old. The price is {price:.2f}"  # using f-string
+print(txt)
+
+txt = f"The price is {20 * 59} dollars"
+print(txt)
+
+txt = "We are the so-called \"Vikings\" from the north."  # using escape character to include double quotes
+print(txt)

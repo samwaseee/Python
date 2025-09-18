7ba8e61e6a85526bdc761d40828a2aaf1486b025
Samiur Rahman Wasee <samwaseee@gmail.com>
2025-09-18 13:02:23 +0600

    Update satistics.py

--- Machine Learning/satistics.py
+++ Machine Learning/satistics.py
@@ -55,7 +55,7 @@ x = np.var(speed)
 # Range
 '''The range is the difference between the highest and lowest values in a set of data.'''
 x = np.ptp(speed)
-# print("Range:", x)
+print("Range:", x)
 
 ages = [5,31,43,48,50,41,7,11,15,39,80,82,32,2,8,6,25,36,27,61,31] 
 
